5fcc0fac9c5ef235440ef201d5f00dd66b88b71f
Xiangzhuang Shen <datahonor@gmail.com>
2025-04-24 16:36:20 +0800

    works

--- toyrl/reinforce.py
+++ toyrl/reinforce.py
@@ -1,8 +1,8 @@
+import gymnasium as gym
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import gymnasium as gym
 
 gamma = 0.99
 
@@ -18,15 +18,16 @@ class Pi(nn.Module):
         self.model = nn.Sequential(*layers)
         self.onpolicy_reset()
         self.train()
-
-    def onpolicy_reset(self):
         self.log_probs = []
         self.rewards = []
-        
+
+    def onpolicy_reset(self) -> None:
+        self.log_probs = []
+        self.rewards = []
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        pdparam = self.model(x)
-        return pdparam
-    
+        return self.model(x)
+
     def act(self, state):
         x = torch.from_numpy(state.astype(np.float32))
         pdparam = self.forward(x)
@@ -46,7 +47,7 @@ def train(pi, optimizer):
         rets[t] = future_ret
     rets = torch.tensor(rets)
     log_probs = torch.stack(pi.log_probs)
-    loss = - log_probs * rets
+    loss = -log_probs * rets
     loss = torch.sum(loss)
     optimizer.zero_grad()
     loss.backward()
@@ -55,14 +56,14 @@ def train(pi, optimizer):
 
 
 def main():
-    env = gym.make('CartPole-v1', render_mode="human")
+    env = gym.make("CartPole-v1", render_mode="human")
     in_dim = env.observation_space.shape[0]
     out_dim = env.action_space.n
     pi = Pi(in_dim, out_dim)
     optimizer = optim.Adam(pi.parameters(), lr=0.01)
     for epi in range(300):
         state, _ = env.reset()
-        for t in range(200):  # cartpole max timestamp is 200
+        for t in range(500):
             action = pi.act(state)
             state, reward, terminated, _, _ = env.step(action)
             pi.rewards.append(reward)
@@ -71,9 +72,10 @@ def main():
                 break
         loss = train(pi, optimizer)
         total_reward = sum(pi.rewards)
-        solved = total_reward > 195.0
+        solved = total_reward > 475.0
         pi.onpolicy_reset()
-        print(f'Episode {epi}, loss: {loss}, total_reward: {total_reward}, solved: {solved}')
+        print(f"Episode {epi}, loss: {loss}, total_reward: {total_reward}, solved: {solved}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
