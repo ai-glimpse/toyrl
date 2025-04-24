18aa38ce3fcb53ea5633c82880a148f6892bc3e3
Xiangzhuang Shen <datahonor@gmail.com>
2025-04-24 17:07:09 +0800

    works

--- toyrl/reinforce.py
+++ toyrl/reinforce.py
@@ -4,10 +4,8 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-gamma = 0.99
 
-
-class Pi(nn.Module):
+class PolicyNet(nn.Module):
     def __init__(self, in_dim: int, out_dim: int) -> None:
         super().__init__()
         layers = [
@@ -16,37 +14,40 @@ class Pi(nn.Module):
             nn.Linear(64, out_dim),
         ]
         self.model = nn.Sequential(*layers)
-        self.onpolicy_reset()
         self.train()
-        self.log_probs = []
-        self.rewards = []
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.model(x)  # type: ignore
+
+
+class Agent:
+    def __init__(self, policy_net: nn.Module) -> None:
+        self.policy_net = policy_net
+        self.log_probs: list[torch.Tensor] = []
+        self.rewards: list[torch.Tensor] = []
 
     def onpolicy_reset(self) -> None:
         self.log_probs = []
         self.rewards = []
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.model(x)
-
     def act(self, state):
         x = torch.from_numpy(state.astype(np.float32))
-        pdparam = self.forward(x)
-        pd = torch.distributions.Categorical(logits=pdparam)
-        action = pd.sample()
-        log_prob = pd.log_prob(action)
+        logits = self.policy_net(x)
+        next_action_dist = torch.distributions.Categorical(logits=logits)
+        action = next_action_dist.sample()
+        log_prob = next_action_dist.log_prob(action)
         self.log_probs.append(log_prob)
         return action.item()
 
 
-def train(pi, optimizer):
-    T = len(pi.rewards)
-    rets = np.empty(T, dtype=np.float32)
+def train(agent: Agent, optimizer: torch.optim.Optimizer, gamma: float = 0.99):
+    T = len(agent.rewards)
+    rets = torch.zeros(T)
     future_ret = 0.0
     for t in reversed(range(T)):
-        future_ret = pi.rewards[t] + gamma * future_ret
+        future_ret = agent.rewards[t].cpu().item() + gamma * future_ret
         rets[t] = future_ret
-    rets = torch.tensor(rets)
-    log_probs = torch.stack(pi.log_probs)
+    log_probs = torch.stack(agent.log_probs)
     loss = -log_probs * rets
     loss = torch.sum(loss)
     optimizer.zero_grad()
@@ -56,24 +57,25 @@ def train(pi, optimizer):
 
 
 def main():
-    env = gym.make("CartPole-v1", render_mode="human")
+    # env = gym.make("CartPole-v1", render_mode="human")
+    env = gym.make("CartPole-v1")
     in_dim = env.observation_space.shape[0]
     out_dim = env.action_space.n
-    pi = Pi(in_dim, out_dim)
-    optimizer = optim.Adam(pi.parameters(), lr=0.01)
-    for epi in range(300):
+    policy_net = PolicyNet(in_dim, out_dim)
+    agent = Agent(policy_net)
+    optimizer = optim.Adam(policy_net.parameters(), lr=0.01)
+    for epi in range(500):
         state, _ = env.reset()
-        for t in range(500):
-            action = pi.act(state)
-            state, reward, terminated, _, _ = env.step(action)
-            pi.rewards.append(reward)
+        terminated, truncated = False, False
+        while not (terminated or truncated):
+            action = agent.act(state)
+            state, reward, terminated, truncated, _ = env.step(action)
+            agent.rewards.append(torch.tensor(reward))
             env.render()
-            if terminated:
-                break
-        loss = train(pi, optimizer)
-        total_reward = sum(pi.rewards)
+        loss = train(agent, optimizer)
+        total_reward = sum(agent.rewards)
         solved = total_reward > 475.0
-        pi.onpolicy_reset()
+        agent.onpolicy_reset()
         print(f"Episode {epi}, loss: {loss}, total_reward: {total_reward}, solved: {solved}")
 
 
