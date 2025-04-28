360e789cef72fa8b8355a320b35d8d4409a63e79
Xiangzhuang Shen <datahonor@gmail.com>
2025-04-28 14:40:11 +0800

    add: import

--- toyrl/__init__.py
+++ toyrl/__init__.py
@@ -1,1 +1,5 @@
-"""MPPT Module."""
+from .reinforce import ReinforceTrainer, ReinforceConfig
+from .sarsa import SarsaTrainer, SarsaConfig
+
+__all__ = ["ReinforceTrainer", "ReinforceConfig", "SarsaTrainer", "SarsaConfig"]
+
