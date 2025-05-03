4ba5b6e1da04adff190a299e58b33b2f9316146b
Mathew Shen <datahonor@gmail.com>
2025-05-03 16:38:46 +0800

    feat: add A2C (#22)

--- toyrl/__init__.py
+++ toyrl/__init__.py
@@ -1,8 +1,11 @@
 from .dqn import DqnConfig, DqnTrainer
 from .reinforce import ReinforceConfig, ReinforceTrainer
 from .sarsa import SarsaConfig, SarsaTrainer
+from .a2c import A2CConfig, A2CTrainer
 
 __all__ = [
+    "A2CConfig",
+    "A2CTrainer",
     "DqnTrainer",
     "DqnConfig",
     "ReinforceTrainer",
