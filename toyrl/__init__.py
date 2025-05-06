e45761f436c79d74db470280104c43e00dfebb1f
Mathew Shen <datahonor@gmail.com>
2025-05-07 00:38:52 +0800

    feat: ppo implementation (#32)

--- toyrl/__init__.py
+++ toyrl/__init__.py
@@ -1,13 +1,16 @@
+from .a2c import A2CConfig, A2CTrainer
 from .dqn import DqnConfig, DqnTrainer
+from .ppo import PPOConfig, PPOTrainer
 from .reinforce import ReinforceConfig, ReinforceTrainer
 from .sarsa import SarsaConfig, SarsaTrainer
-from .a2c import A2CConfig, A2CTrainer
 
 __all__ = [
     "A2CConfig",
     "A2CTrainer",
     "DqnTrainer",
     "DqnConfig",
+    "PPOConfig",
+    "PPOTrainer",
     "ReinforceTrainer",
     "ReinforceConfig",
     "SarsaTrainer",
